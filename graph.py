d3e992c283f9214e958b24044583ff504f174d62
FlorimGaina <florinelgaina@yahoo.com>
2023-03-12 21:48:04 +0200

    read

--- graph.py
+++ graph.py
@@ -5,18 +5,22 @@ class DirectedGraph:
         self.__dictionary_predecessor = {}
         self.__dictionary_successor = {}
         self.__dictionary_cost = {}
-        self.__read_standard_format("graph1.txt")
+        self.read_standard_format("graph1.txt")
 
-    def __read_standard_format(self, file_name):
+    def read_standard_format(self, file_name):
         file = open(file_name, "r")
         first_line = file.readline()
         try:
             n, m = first_line.split(" ")
-        except ValueError:
+        except Exception:
             return
 
         lines = file.readlines()
         for line in lines:
-            pass
+            #TODO check for already existing vertexes using is_Vertex
+            x, y, cost = line.split(" ")
+            self.__dictionary_cost[(x, y)] = cost
+            self.__dictionary_successor[x] = y
+            self.__dictionary_predecessor[y] = x
 
 
