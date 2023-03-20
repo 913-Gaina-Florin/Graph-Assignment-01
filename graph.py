f61412d6ca8a3315abbefe10887ce765bd5c476b
FlorimGaina <florinelgaina@yahoo.com>
2023-03-20 11:52:35 +0200

    read

--- graph.py
+++ graph.py
@@ -6,12 +6,16 @@ class DirectedGraph:
         self.__dictionary_predecessor = {}
         self.__dictionary_successor = {}
         self.__dictionary_cost = {}
-        self.read_standard_format(file_name)
 
     def print_graphh(self):
         # test function
         for key in self.__dictionary_cost.keys():
             print(key, self.__dictionary_cost[key])
+
+    def __clear_memory(self):
+        self.__dictionary_predecessor.clear()
+        self.__dictionary_successor.clear()
+        self.__dictionary_cost.clear()
 
     def is_vertex(self, vertex):
         if vertex in self.__dictionary_predecessor.keys():
@@ -36,15 +40,61 @@ class DirectedGraph:
     def get_number_edges(self):
         return len(self.__dictionary_cost)
 
+    def get_vertices_iterable(self):
+        vertices_array = []
+        for key in self.__dictionary_predecessor.keys():
+            vertices_array.append(key)
+        return vertices_array
+
+    def change_edge_cost(self, u, v, cost):
+        if self.is_edge(u, v) is False:
+            return
+        self.__dictionary_cost[(u, v)] = cost
+
+    def get_outbound_edges_iterable(self, vertex):
+        edges_array = []
+        if not self.is_vertex(vertex):
+            return None
+        for adj_vertex in self.__dictionary_successor[vertex]:
+            edges_array.append((vertex, adj_vertex))
+        return edges_array
+
+    def get_inbound_edges_iterable(self, vertex):
+        edges_array = []
+        if not self.is_vertex(vertex):
+            return None
+        for adj_vertex in self.__dictionary_predecessor[vertex]:
+            edges_array.append((adj_vertex, vertex))
+        return edges_array
+
     def get_out_degree(self, vertex):
         if not self.is_vertex(vertex):
             return None
         return len(self.__dictionary_successor[vertex])
 
     def is_edge(self, u, v):
-        if (u, v) in self.__dictionary_cost.keys():
-            return True
-        return False
+        if not self.is_vertex(u) or not self.is_vertex(v):
+            return False
+
+        found = False
+        for vertex_predecessor in self.__dictionary_predecessor[v]:
+            if vertex_predecessor == u:
+                found = True
+                break
+
+        if found is False:
+            return False
+
+        found = False
+        for vertex_successor in self.__dictionary_successor[u]:
+            if vertex_successor == v:
+                found = True
+                break
+
+        if found is False:
+            return False
+
+        return True
 
     def add_vertex(self, vertex):
         if not self.is_vertex(vertex):
@@ -64,17 +114,22 @@ class DirectedGraph:
         return copy.deepcopy(self)
 
     def read_standard_format(self, file_name):
+        self.__clear_memory()
+
         file = open(file_name, "r")
         first_line = file.readline()
         try:
-            n, m = first_line.split(" ")
+            n, m = first_line.strip('\n').split(" ")
         except Exception:
             print("Error reading the file. ")
             return
 
+        for vertex in range(int(n)):
+            self.add_vertex(str(vertex))
+
         lines = file.readlines()
         for line in lines:
-            x, y, cost = line.split(" ")
+            x, y, cost = line.strip('\n').split(" ")
             self.add_vertex(x)
             self.add_vertex(y)
             self.add_edge(x, y, cost)
--- main.py
+++ main.py
@@ -10,6 +10,15 @@ def print_options():
     print("5 - Check edge existence. ")
     print("6 - Get a cost associated to an edge. ")
     print("7 - Print the graph to the screen. ")
+    print("8 - Iterate through the set of vertices. ")
+    print("9 - Iterate through the set of outbound edges of a vertex. ")
+    print("10 - Iterate through the set of inbound edges of a vertex. ")
+    print("11 - Make a copy of the graph. ")
+    print("12 - Get the number of vertices. ")
+    print("13 - Change the cost associated to an edge. ")
+    print("14 - Read graph standard format. ")
+    print("15 - Read graph special format. ")
+    print("16 - Save graph special format. ")
 
 
 def main_loop(input_graph: DirectedGraph):
@@ -39,13 +48,21 @@ def main_loop(input_graph: DirectedGraph):
 
         elif user_option == "3":
             vertex = input("Enter the vertex: ")
-            input_graph.add_vertex(vertex)
+
+            if input_graph.is_vertex(vertex) is True:
+                print(f'{vertex} already exists! ')
+            else:
+                input_graph.add_vertex(vertex)
 
         elif user_option == "4":
             u = input("Enter the first vertex: ")
             v = input("Enter the second vertex: ")
             cost = input("Enter the cost: ")
-            input_graph.add_edge(u, v, int(cost))
+
+            if input_graph.is_edge(u, v) is True:
+                print(f'{(u, v)} already exists! ')
+            else:
+                input_graph.add_edge(u, v, int(cost))
 
         elif user_option == "5":
             u = input("Enter the first vertex: ")
@@ -65,7 +82,63 @@ def main_loop(input_graph: DirectedGraph):
         elif user_option == "7":
             input_graph.print_graphh()
 
+        elif user_option == "8":
+            print("The set of vertices is: ")
+            print(input_graph.get_vertices_iterable())
+
+        elif user_option == "9":
+            vertex = input("Enter the vertex: ")
+            iterable = input_graph.get_outbound_edges_iterable(vertex)
+            print("The outbound edges are: ")
+
+            if iterable is None:
+                print(f'{vertex} is not a valid vertex! ')
+            elif len(iterable) == 0:
+                print(f'{vertex} has no outbound edges! ')
+            else:
+                print(iterable)
+
+        elif user_option == "10":
+            vertex = input("Enter the vertex: ")
+            iterable = input_graph.get_inbound_edges_iterable(vertex)
+            print("The inbound edges are: ")
+
+            if iterable is None:
+                print(f'{vertex} is not a valid vertex! ')
+            elif len(iterable) == 0:
+                print(f'{vertex} has no inbound edges! ')
+            else:
+                print(iterable)
+
+        elif user_option == "11":
+            copy_object = input_graph.make_copy()
+            print("The address of the original graph is: ", input_graph)
+            print("The address of the copy is : ", copy_object)
+
+        elif user_option == "12":
+            print(input_graph.get_number_vertices())
+
+        elif user_option == "13":
+            u = input("Enter the first vertex: ")
+            v = input("Enter the second vertex: ")
+            cost = input("Enter the cost: ")
+
+            if input_graph.is_edge(u, v) is True:
+                input_graph.change_edge_cost(u, v, cost)
+            else:
+                print(f'The edge {(u, v)} does not exist! ')
+
+        elif user_option == "14":
+            file_name = input("Enter the file name: ")
+            input_graph.read_standard_format(file_name)
+
+        elif user_option == "15":
+            pass
+
+        elif user_option == "16":
+            pass
+
 
 if __name__ == '__main__':
-    graph = DirectedGraph("graph1.txt")
+    graph = DirectedGraph("graph1k.txt")
     main_loop(graph)
